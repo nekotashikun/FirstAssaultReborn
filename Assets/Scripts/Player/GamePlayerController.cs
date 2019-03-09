1a7eb2b5b392fb1accf5e8d7bd43dd1736f1120d
Matt Jones <matthewj866@gmail.com>
2019-03-09 21:45:36 +0100

    Rider plugin is being weird again - readded. Added basic slidey capsule movement.

--- Assets/Scripts/Player/GamePlayerController.cs
+++ Assets/Scripts/Player/GamePlayerController.cs
@@ -2,17 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GamePlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] 
+    private Rigidbody _playerRb;
+
+    [SerializeField] 
+    private float _walkingSpeed;
+    
+    private Vector3 _inputDirection;
+    private Vector3 _previousInputDirection;
+
+    private void FixedUpdate()
     {
+        Debug.Log(_inputDirection);
+        if (_inputDirection == Vector3.zero)
+        {
+            _previousInputDirection = _inputDirection;
+            return;
+        }
+
+        float resultDirectionSpeed =
+            (1 / (Mathf.Abs(_inputDirection.x) + Mathf.Abs(_inputDirection.z))) * _walkingSpeed;
         
+        _playerRb.AddRelativeForce(_inputDirection * resultDirectionSpeed, ForceMode.VelocityChange);
+        
+        _previousInputDirection = _inputDirection;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            _inputDirection.z = 1f;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            _inputDirection.z = -1f;
+        }
+        else
+        {
+            _inputDirection.z = 0;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _inputDirection.x = 1f;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            _inputDirection.x = -1f;
+        }
+        else
+        {
+            _inputDirection.x = 0;
+        }
     }
 }
