76bed497faf210bf5eb3f46d6b63c3c457df7356
Matt Jones <matthewj866@gmail.com>
2019-03-12 21:19:11 +0100

    Ported message broker/game messenger.

---
+++ Assets/Scripts/Utilities/MessageBroker/GameMessenger.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utilities.MessageBroker
+{
+    public class GameMessenger : PocoSingleton<GameMessenger>
+    {
+        private Dictionary<Type, List<Delegate>> _messageMappings;
+
+        public GameMessenger()
+        {
+            _messageMappings = new Dictionary<Type, List<Delegate>>();
+        }
+
+        public void RegisterSubscriberToMessageTypeOf<T>(Action<T> messageHandler)
+        {
+            var messageType = typeof(T);
+            if(!_messageMappings.ContainsKey(messageType))
+            {
+                _messageMappings.Add(messageType, new List<Delegate>());
+            }
+            
+            _messageMappings[messageType].Add(messageHandler);
+        }
+
+        public void SendMessageOfType<T>(T message)
+        {
+
+            var messageType = typeof(T);
+            if (!_messageMappings.ContainsKey(messageType)) return;
+
+            foreach (var listener in _messageMappings[messageType])
+            {
+                listener.DynamicInvoke(message);
+            }
+        }
+
+        public void UnRegisterAllMessagesForObject(object targetObject)
+        {
+            var listsOfActions = _messageMappings.Values;
+            foreach (var listOfActions in listsOfActions)
+            {
+                listOfActions.RemoveAll(x => x.Target == targetObject);
+            }
+        }
+    }
+}
