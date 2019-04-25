17cde6caf2a6a2e236e0152c615a39de50e11884
Matt Jones <matthewj866@gmail.com>
2019-04-25 19:41:34 +0200

    Modified base weapon logic. Added Tac Reload prototype component.

---
+++ Assets/Scripts/Weapons/Prototypes/BasicTacReloadHitscanWeapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Weapons.Prototypes;
+
+namespace Weapons.Prototypes
+{
+    public class BasicTacReloadHitscanWeapon : BasicHitscanWeapon
+    {
+        [SerializeField]
+        private string _tacReloadAnimParameter = string.Empty;
+
+        [SerializeField]
+        private int _maxCapacity = 30;
+
+        private int _currentCapacity = 0;
+
+        public override string ReloadAnimParameter => _currentCapacity <= 0 ? _tacReloadAnimParameter : _reloadAnimParameter;
+
+        private void Start()
+        {
+            _currentCapacity = _maxCapacity;
+        }
+
+        public override bool Fire()
+        {
+            bool didFire = base.Fire();
+
+            if (didFire) --_currentCapacity;
+
+            return didFire;
+        }
+    }
+}
