609eb6b1a07c0101f7a78e646b17d2241c62a287
Renato Matosinho <therenatobr@gmail.com>
2019-05-05 05:29:59 -0300

    Removed camera from player prefab, now it tracks the local player completely by script; Added a maximum speed for camera tracking on Y axis, so crouching doesn't feel like a snap;

--- Assets/Scripts/Player/Balin/LocalCamera.cs
+++ Assets/Scripts/Player/Balin/LocalCamera.cs
@@ -8,6 +8,7 @@ public class LocalCamera : MonoBehaviour
 {
 
     public TestMovement localPlayer;
+    public float maxVerticalSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +19,15 @@ public class LocalCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 newPosition = transform.position;
+        newPosition.x = localPlayer.transform.position.x;
+        newPosition.z = localPlayer.transform.position.z;
+        newPosition.y = Mathf.MoveTowards(newPosition.y, localPlayer.transform.position.y + 0.525f, maxVerticalSpeed * Time.deltaTime);
+        transform.position = newPosition;
+
         Vector3 newRotation = transform.localEulerAngles;
         newRotation.x = localPlayer.lookAngle * -1;
+        newRotation.y = localPlayer.transform.eulerAngles.y;
         transform.localEulerAngles = newRotation;
     }
 }
